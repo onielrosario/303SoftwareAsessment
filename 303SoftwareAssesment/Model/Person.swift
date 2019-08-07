5cf561442e9fafb36e9901eb53376c304bf3b24b
onielrosario <onielrosario@pursuit.org>
2019-08-07 12:41:59 -0600

    Model created for JSON data

---
+++ 303SoftwareAssesment/Model/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  303SoftwareAssesment
+//
+//  Created by Oniel Rosario on 8/7/19.
+//  Copyright © 2019 Oniel Rosario. All rights reserved.
+//
+
+import Foundation
+
+
+//json model
+class Person: Codable {
+    let fn: String
+    let ln: String
+    let city: String
+}
