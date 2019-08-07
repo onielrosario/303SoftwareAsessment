119ebd36347680e3d67e5d6bc46b3393c371d504
onielrosario <onielrosario@pursuit.org>
2019-08-07 13:19:35 -0600

    refactoring for cleaner code

--- 303SoftwareAssesment/API Client/APIClient.swift
+++ 303SoftwareAssesment/API Client/APIClient.swift
@@ -13,9 +13,21 @@ import Foundation
 final class APIClient {
     //get method
     static func GetJSONData(completionHandler: @escaping([Person]?, Error?)-> Void) {
+        //url returns an array of people data (firtname, lastname, location city)
+        guard let url = URL(string: "http://www.filltext.com/?rows=100&fname=%7BfirstName%7D&lname=%7BlastName%7D&city=%7Bcity%7D&pretty=true") else { return }
+        URLSession.shared.dataTask(with: url) { (data, response ,error)  in
+            if let error = error {
+                //catch the error
+                completionHandler(nil, error)
+            } else if let data = data {
+                do {
+                    let people = try JSONDecoder().decode([Person].self, from: data)
+                    //got data
+                    completionHandler(people, nil)
+                } catch {
+                    completionHandler(nil, error)
+                }
+            }
+        }.resume()
     }
-    
-    
-    
-    
 }
