06de7d654f6d2e3608ea562688e41f48b3add7db
onielrosario <onielrosario@pursuit.org>
2019-08-07 12:27:49 -0600

    first commit

---
+++ 303SoftwareAssesmentTests/_03SoftwareAssesmentTests.swift
@@ -0,0 +1,24 @@
+//
+//  _03SoftwareAssesmentTests.swift
+//  303SoftwareAssesmentTests
+//
+//  Created by Oniel Rosario on 8/7/19.
+//  Copyright © 2019 Oniel Rosario. All rights reserved.
+//
+
+import XCTest
+@testable import _03SoftwareAssesment
+
+class _03SoftwareAssesmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+ 
+
+}
