09b3bfde5d6d7708e3c1d43f64e8588feaf296eb
onielrosario <onielrosario@pursuit.org>
2019-08-07 14:24:13 -0600

    remembered to add unit testing to make sure I'm getting data

--- 303SoftwareAssesmentTests/_03SoftwareAssesmentTests.swift
+++ 303SoftwareAssesmentTests/_03SoftwareAssesmentTests.swift
@@ -19,6 +19,16 @@ class _03SoftwareAssesmentTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
- 
-
+    func testGetJSONData() {
+        let exp = expectation(description: "got data")
+       guard let url = URL(string: "http://www.filltext.com/?rows=100&fname=%7BfirstName%7D&lname=%7BlastName%7D&city=%7Bcity%7D&pretty=true") else { return }
+        URLSession.shared.dataTask(with: url) { (data, response ,error)  in
+            if let error = error {
+               XCTFail("error in the url \(error)")
+            } else if let _ = data {
+               exp.fulfill()
+                }
+            }.resume()
+        wait(for: [exp], timeout: 3.0)
+    }
 }
